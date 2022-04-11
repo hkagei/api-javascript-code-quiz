a9f36e9e42115cc0e90d542f3209e298b69d5d23
hkagei <k_dawg_1022@hotmail.com>
2022-04-10 18:28:36 -0600

    add the questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,40 @@ var timerEl = document.querySelector("#timeLeft")
 var startQuiz = document.querySelector("#start-quiz")
 
 
+var questions = [
+    {
+        question: "The condition statement if/else is enclosed with the following:",
+        choices: ["Parentheses", "Curly Brackets", "Quotes", "Square Brackets"],
+        answer: "Parentheses"
+
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["Numbers and strings", "Booleans", "Other arrays", "All of the above"],
+        answer: "All of the above"
+
+    },
+    {
+        question: "Commonly used datatypes include the following, except _____.",
+        choices: ["Strings", "Boolean", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        question: "A very useful tool to debug arrays is:",
+        choices: ["Javascript", "Terminal/Bash", "For Loops", "Console Log"],
+        answer: "For Loops"
+    },
+    {
+        question: "Strings must be enclosed with",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+        answer: "Quotes"
+    },
+
+];
+var question1 = document.getElementById('question1');
+
+question1.textContent = questions[0].question;
+
 // function that will start the quiz
 startQuiz.addEventListener("click", function(){
     countdown();
@@ -31,8 +65,6 @@ function countdown() {
       }
     }, 1000);
   }
-var question1 = document.getElementById('question1');
-  question1.textContent = questions[0].question;
 
 //save_score.addEventListener("click", function() {})
     
